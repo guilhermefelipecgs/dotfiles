7d43ad265b9b23696a081963e6fc920d00f420db
Guilherme Felipe <guilhermefelipecgs@gmail.com>
2014-08-29 20:22:17 -0300

    Adding installation requirements

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # System Requirements
-- Exuberant Ctags
 - Python 2.7.\* or 3.4.\*
-- [The Silver Searcher](https://github.com/ggreer/the_silver_searcher)
 
--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sudo apt-get update
+sudo apt-get install exuberant-ctags
+sudo apt-get install silversearcher-ag
+
 if [ ! -d "$HOME/.vim" ]; then
   mkdir "$HOME/.vim"
 fi
